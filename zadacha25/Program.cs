34b29fa5aa46e58050f39799c7e2b9991a8ad03d
Александр Чехонин <2007cheh@mail.ru>
2022-11-25 10:14:50 +0300

    zadacha25

--- zadacha25/Program.cs
+++ zadacha25/Program.cs
@@ -2,3 +2,21 @@
 //            два числа (A и B) и возводит число A в натуральную степень B.
 //            Math.Pow использовать нельзя.
 
+Console.WriteLine();
+int Exp(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int exp = Exp(A, B);
+Console.WriteLine("Ответ: " + exp);
